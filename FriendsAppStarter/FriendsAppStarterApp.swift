6dbcad3d924587068c1a97175c51d9717a189bdc
snowlvv <rhn.owv1027@gmail.com>
2025-05-06 00:38:10 +0800

    Initial Commit

---
+++ FriendsAppStarter/FriendsAppStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsAppStarterApp.swift
+//  FriendsAppStarter
+//
+//  Created by 유휘나 on 2025/05/06.
+//
+
+import SwiftUI
+
+@main
+struct FriendsAppStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
